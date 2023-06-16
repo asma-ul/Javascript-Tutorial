d01b80229ba844bdc64a0d1e244019dddad3e704
Asmaul Husna <asmaulhusna3777@gmail.com>
2023-06-16 09:20:41 +0600

    less2 add here

---
+++ Less-2/main.js
@@ -0,0 +1,67 @@
+// var num = "20.5"
+
+// num = parseFloat(num);
+
+// console.log(typeof(num));
+
+
+// var number = 2.5678
+
+// console.log(number.toPrecision(4))
+
+// console.log(typeof(Number("123")));
+// console.log(typeof(Number("12.3")));
+// console.log(Number(flase));
+
+
+// var num = 20;
+// console.log(typeof(num))
+
+// var num = 20;
+// num = toString(num);
+
+// console.log(typeof(num));
+
+
+// var num ="20";
+// num = parseInt(num);
+// console.log(typeof(num));
+
+
+// var num = "20.13";
+// num = parseFloat(num);
+// console.log(typeof(num));
+
+
+// var number = 1.4567;
+// console.log(number.toFixed(2));
+// console.log(number.toPrecision(2));
+
+// console.log(typeof(Number("1234")));
+// console.log(typeof(Number("12.34")));
+// console.log(Number(true));
+
+var num = 20;
+console.log(typeof(num));
+
+var num = 20;
+var num = toString(num);
+console.log(typeof(num));
+
+var num = 20;
+var num = parseInt(num);
+console.log(typeof(num));
+
+var num = 3.456;
+var num = parseFloat(num);
+console.log(typeof(num));
+
+var number = 4.4567;
+console.log(number.toFixed(2));
+
+var number = 2.1234;
+console.log(number.toPrecision(3));
+
+console.log(typeof(Number("1234")));
+console.log(typeof(Number("12.34")));
+console.log(Number(true));
---
+++ Less-3/main.js
@@ -0,0 +1,15 @@
+document.write("Anisul" + " Islam" + " Rubel");
+
+var firstName = "Anisul";
+
+var lastName = "Islam";
+var fullName = firstName + lastName;
+document.write("my name is" + fullName + "anisul islam my name is");
+
+var fullName = "Anisul Islam";
+document.write(fullName + " is my name");
+
+var num1 = 20;
+var num2 = 30;
+
+document.write("Num1 = " + num1 + " and Num2 = " + num2 )
